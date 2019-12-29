9fc505d6da357fe5156153a12516c717cca31d4f
collinsctk@qytang.com <collinsctk@qytang.com>
2019-12-29 18:38:31 +0800

    更新为最终脚本!

--- ec2_script.sh
+++ ec2_script.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-yum groupinstall -y Development tools
-yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel gcc
-yum install wget
-wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz
-xz -d Python-3.6.8.tar.xz
-tar xvf Python-3.6.8.tar
-cd Python-3.6.8
-./configure --prefix=/usr/local
-make && make install
-ln -s /usr/local/bin/python3 /usr/bin/python3
-ln -s /usr/local/bin/pip3 /usr/bin/pip3
+yum install -y python36 python36-virtualenv python36-pip git
+ln -s /usr/bin/pip-3.6 /usr/bin/pip3
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sed -i "s/UsePAM yes/UsePAM no/g" /etc/ssh/sshd_config
+> ~/.ssh/authorized_keys
+sudo sh -c 'echo root:Cisc0123 | chpasswd'
+service sshd restart
 cd /
+yum install -y git
 git clone https://gitee.com/qytang/AWS.git
 pip3 install boto3
 pip3 install flask
+yum install -y gcc
+pip3 install uwsgi
 mkdir ~/.aws/
 echo '[default]' > ~/.aws/config
 echo 'region=ap-northeast-2' >> ~/.aws/config
 python3 /AWS/insert_db_1_create_table.py
 python3 /AWS/insert_db_2_insert.py
 python3 /AWS/upload_files.py
-python3 /AWS/app.py &
+cp /AWS/uwsgi.conf /etc/init/uwsgi.conf
+start uwsgi
+yum install -y nginx
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf-bak
+cp /AWS/nginx.conf /etc/nginx/nginx.conf
+service nginx start
+chkconfig nginx on
