720f4907841fd718288b99449028e58b7079cfd7
collinsctk@qytang.com <collinsctk@qytang.com>
2019-12-30 18:15:15 +0800

    更新为最终脚本!

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import os
 from insert_db_3_get_item import get_all_item
+import requests
+
 # 默认目录为当前目录的templates
 template_dir = os.path.abspath('/AWS/templates')
 
@@ -10,7 +12,13 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return render_template('index.html', devnet_main='乾颐堂AWS测试', active='首页')
+    instance_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id").text
+    availability_zone = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone").text
+    return render_template('index.html',
+                           devnet_main='乾颐堂AWS测试',
+                           instance_id = instance_id,
+                           availability_zone = availability_zone,
+                           active='首页')
 
 @app.route('/staff_info')
 def staff_info():
