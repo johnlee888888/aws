88810afbea751a0d0a050f430894c304a1c5ebf6
john <ydbook@ymail.com>
2020-03-31 11:07:54 +0800

    update service

---
+++ myweb.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import os
+from insert_db_3_get_item import get_all_item
+import requests
+
+# 默认目录为当前目录的templates
+template_dir = os.path.abspath('/aws/templates')
+
+application = Flask(__name__, template_folder=template_dir)
+
+application.debug = True
+
+@application.route('/')
+def index():
+    instance_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id").text
+    availability_zone = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone").text
+    return render_template('index.html',
+                           devnet_main='乾颐堂AWS测试',
+                           instance_id = instance_id,
+                           availability_zone = availability_zone,
+                           active='首页')
+
+@application.route('/staff_info')
+def staff_info():
+    staff_list = get_all_item('staff')
+    return render_template('staff.html', staff_list=staff_list, active='员工信息')
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
